01c0d6a88565c243fd6c1aceba9d0200ffe4a0e7
yosmisyael <yosmisyael7308@gmail.com>
2025-07-15 16:02:28 +0700

    feat: craete header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import {AlignJustify, ChevronDown} from "lucide-react";
+import { useState } from "react";
+
+export default function Header() {
+    const [dropDown, setDropDown] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    return (
+        <nav className="navbar">
+            <div className="flex gap-16">
+                {/* brand */}
+                <div>
+                    <h3 className="font-bold text-2xl font-secondary">Notease</h3>
+                </div>
+
+                {/* dropdown */}
+                {
+                    mobileMenu && (
+                        <ul className="dropdown top-0 left-0 bottom-0 w-full">
+                        <li><a href="">Get Started</a></li>
+                        <li className="inline-flex flex-col gap-5">
+                            <span className="inline-flex items-center gap-2 hover:cursor-pointer" onClick={() => setDropDown(!dropDown)}>
+                                Features
+                                <ChevronDown size={24} strokeWidth={3} className={`pt-[6px] transition-all duration-200 ease-out ${dropDown ? 'rotate-180 translate-y-1' : ''}`}/>
+                            </span>
+                            { dropDown && (
+                                <ul className="relative lg:absolute lg:bg-background lg:top-[100%] lg:translate-y-7 flex flex-col gap-8 text-2xl lg:text-xl font-bold font-primary [&>:first-child]:pt-4 lg:[&>:first-child]:pt-0 px-4 lg:px-3 lg:py-2 lg:rounded-lg lg:border-2 lg:border-primary">
+                                    <li>Note</li>
+                                    <li>Tag</li>
+                                    <li>Secure note</li>
+                                </ul>
+                            ) }
+                        </li>
+                        <li><a href="">How it Works?</a></li>
+                        <li><a href="">Contribute</a></li>
+                    </ul>
+                    )
+                }
+            </div>
+
+            {/* cta */}
+            <div className="flex gap-2 relative z-10">
+                <a href="" className="hidden md:block btn-primary">Sign up</a>
+                <a href="" className="hidden md:block btn-secondary">Log in</a>
+                <a href="" className="md:hidden btn-primary">Get Notease</a>
+                <button type="button" className="lg:hidden hover:cursor-pointer" onClick={() => setMobileMenu(!mobileMenu)}><AlignJustify /></button>
+            </div>
+        </nav>
+    )
+}
