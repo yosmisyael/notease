3780effea4405c395ed4e4e71f9ccf60a587d2e1
yosmisyael <yosmisyael7308@gmail.com>
2025-07-15 16:16:48 +0700

    fix: fix dropdown bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,36 @@
 "use client"
 
 import {AlignJustify, ChevronDown} from "lucide-react";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
 export default function Header() {
-    const [dropDown, setDropDown] = useState(false);
-    const [mobileMenu, setMobileMenu] = useState(false);
+    const [dropDown, setDropDown] = useState<boolean>(false);
+    const [mobileMenu, setMobileMenu] = useState<boolean>(false);
+    const [screenWidth, setScreenWidth] = useState<number>(0);
+
+    useEffect(() => {
+       function handleResize() {
+           setScreenWidth(window.innerWidth);
+       }
+
+       window.addEventListener('resize', handleResize);
+
+       handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     return (
         <nav className="navbar">
-            <div className="flex gap-16">
+            <div className="flex gap-6">
                 {/* brand */}
-                <div>
+                <div className="">
                     <h3 className="font-bold text-2xl font-secondary">Notease</h3>
                 </div>
 
                 {/* dropdown */}
                 {
-                    mobileMenu && (
+                    ((mobileMenu && screenWidth < 1024) || screenWidth >= 1024) && (
                         <ul className="dropdown top-0 left-0 bottom-0 w-full">
                         <li><a href="">Get Started</a></li>
                         <li className="inline-flex flex-col gap-5">
@@ -45,7 +58,9 @@ export default function Header() {
                 <a href="" className="hidden md:block btn-primary">Sign up</a>
                 <a href="" className="hidden md:block btn-secondary">Log in</a>
                 <a href="" className="md:hidden btn-primary">Get Notease</a>
-                <button type="button" className="lg:hidden hover:cursor-pointer" onClick={() => setMobileMenu(!mobileMenu)}><AlignJustify /></button>
+                <button type="button" className="lg:hidden hover:cursor-pointer" onClick={() => setMobileMenu(!mobileMenu)}>
+                    <AlignJustify />
+                </button>
             </div>
         </nav>
     )
